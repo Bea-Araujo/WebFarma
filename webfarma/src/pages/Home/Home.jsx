04751a3cda8c6706cc155a7b48536d0477546850
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-08-10 00:09:09 -0300

    page cards estilo atualizado

--- webfarma/src/pages/Home/Home.jsx
+++ webfarma/src/pages/Home/Home.jsx
@@ -2,25 +2,32 @@ import React from 'react'
 import s from './Home.module.css'
 import arrow1 from '../../assets/Arrow1.svg'
 import arrow2 from '../../assets/Arrow2.svg'
+import farma1 from '../../assets/img-farma.jpg'
+import farmaceuticos from '../../assets/farmaceuticos.png'
+import Medicamentos from '../../assets/Medicamentos.png'
+import categoria from '../../assets/categoria.png'
+
+import { PageCard } from '../../components/PageCard/PageCard'
 
 export const Home = () => {
     return (
         <main className={s.container}>
-            <section className={s.carousel}>
-                <button>
-                    <img src={arrow1} alt="" style={{ paddingLeft: '40px' }} />
-                </button>
-                <div className={s.images}>
-                    <img src="" alt="" />
-                    <div>
-                        {/*  */}
-                        <p>a</p>
-                    </div>
-                </div>
-                <button>
-                    <img src={arrow2} alt="" style={{ paddingRight: '40px' }} />
-                </button>
+            <section className={s.intro}>
+                <article>
+                    <h2>Welcome to WebFarma,</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, reprehenderit? Ut consequuntur repellat adipisci odit dolores, saepe dolorem quidem laboriosam delectus rerum autem quibusdam maiores, nulla incidunt ipsum illum perferendis.</p>
+                </article>
+                <picture>
+                    <img src={farma1} alt="" />
+                </picture>
             </section>
+
+            <section className={s.pageSection}>
+                <PageCard img={categoria} title='Medicamentos' path='/products' />
+                <PageCard img={Medicamentos} title='Categoria' path='/' />
+                <PageCard img={farmaceuticos} title='Farmacêuticos' path='/' />
+            </section>
+
         </main>
     )
 }
